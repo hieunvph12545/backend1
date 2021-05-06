e97eb94076cb0316f7070eecc997418b31d3d0b6
HieuNv <hieunvmob@gmail.com>
2021-05-06 18:13:10 +0700

    update

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,44 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+const mysql      = require('mysql');
+const connection = mysql.createConnection({
+  host     : 'localhost',
+  user     : 'root',
+  password : 'password',
+  database : 'search_license_plate'
+});
+connection.connect(function(err) {
+    if (err) {
+  console.error('error connecting: ' + err.stack);
+  return;
+}
 
-/* GET home page. */
+console.log('connected');
+});
+
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index')
+});
+/* GET home page. */
+router.get('/getData', function(req, res) {
+  var baseJson = {
+    errorCode: undefined,
+    errorMessage: undefined,
+    data: undefined
+  }
+  connection.query('SELECT * FROM license_plates', function (error, results, fields) {
+    if (error) {
+      baseJson.errorCode = 403
+      baseJson.errorMessage = '403 Forbidden'
+      baseJson.data = []
+    } else {
+      baseJson.errorCode = 200
+      baseJson.errorMessage = 'OK'
+      baseJson.data = results
+    }
+    res.send(baseJson);
+  });
+
 });
 
 module.exports = router;
